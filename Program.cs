23485541bccdc4d6c280b5b96669874a39719667
Nikita Shalginskih <n.shalginskih@nc.rt.spb>
2022-06-22 17:09:57 +0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая на вход
+// принимает два числа и выдаёт, какое число большее, а
+// какое меньшее.
+{int a, b;
+Console.WriteLine("Введите два числа : ");
+a = int.Parse(Console.ReadLine());
+b = int.Parse(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine("Минимальное число {0}", a);
+
+}
+else
+{
+    Console.WriteLine("Максимальное число {0}", b);
+}
+Console.ReadLine();}
